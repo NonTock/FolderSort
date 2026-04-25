64fcc088a551a830e497ef33b768dbd60f9cbd5e
NonTock <62567734+NonTock@users.noreply.github.com>
2026-04-25 08:25:41 +0300

    Sorted list is work, checking for others files

--- main.py
+++ main.py
@@ -8,17 +8,43 @@ sort_checker = {
     "Images": [".jpg", ".jpeg", ".png", ".gif"],
     "Documents": [".pdf", ".doc", ".docx", ".odt", ".txt", ".xlsx"],
     "Videos": [".mp4", ".avi", ".mkv"],
-    "Archives": [".zip", ".rar", ".tar.gz", ".7z", ".tar.xz"],
+    "Archives": [".zip", ".rar", ".tar.gz", ".7z", ".tar.xz", ".deb"],
     "Executables": [".exe", ".msi", ".dmg"],
-    "Others": [],
+    "Torrents": [".torrent"],
+    "Programming": [".py"],
+    "Others": [".a"],
 }
+
+
 def start():
     print("Это сортировщик файлов в папке")
     sleep(1)
     print("Не сортируй системные файлы, это может вызвать ошибки")
-    print("Отправь ссылку на сортируемую папку в формате: 'C:/users/userName/Downloads'")
-    uri = input("URI: ")
+    print("Отправь путь на сортируемую папку в формате: 'C:/users/username/Downloads'")
+    uri = input("Путь: ")
     while Path(uri).is_dir() == False:
         print("Не корректная ссылка")
-        uri = input("URI: ")
+        uri = input("Путь: ")
+    sorted_list(uri)
+
+def available_check(file):
+    for value in sort_checker.values():
+        if file in value:
+            break
+    else:
+        sort_checker["Others"].append(file)
+
+
+def sorted_list(uri):
+    uri = Path(uri)
+    files_uri = uri.iterdir()
+    for file in files_uri:
+        if file.is_file():
+            available_check(file.suffix)
+            for key in sort_checker:
+                for value in sort_checker[key]:
+                    if value == file.suffix:
+                        flag = 1
+                        print(f"{file.name} - {key}")
+                        break
 start()
